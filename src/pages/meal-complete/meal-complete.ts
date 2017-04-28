bf20a47a44e49b731886077e6eedec217ad89a1c
Vanessa Frost <vafrost@ncsu.edu>
2017-04-28 14:42:02 -0400

    re-added import

--- src/pages/meal-complete/meal-complete.ts
+++ src/pages/meal-complete/meal-complete.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Events } from 'ionic-angular';
 import { MainPage } from '../../pages/pages';import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
 
 // Import ionic2-rating module
 import { Ionic2RatingModule } from 'ionic2-rating';
