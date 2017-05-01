80ce4439d5a816fc6508347b94878a328c6e6b59
Chris Elias <chris.elias5@gmail.com>
2017-05-01 13:06:03 -0400

    Made place holders for the suggested dishes

--- src/pages/dish-added/dish-added.ts
+++ src/pages/dish-added/dish-added.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, ViewController } from 'ionic-angular';
 import { Events } from 'ionic-angular';
 
+import { Api } from '../../providers/api';
+
 import { SelectDish } from '../select-dish/select-dish';
 
 @Component({
@@ -10,14 +12,37 @@ import { SelectDish } from '../select-dish/select-dish';
 })
 export class DishAdded {
 
+  dishResults: any = [];
+
   constructor(
     private navCtrl: NavController,
     private viewCtrl: ViewController,
-    private events: Events
-  ) {}
+    private events: Events,
+    private api: Api,
+  ) {
+    this.getDishes("");
+  }
 
   goToMainPage() {
     this.viewCtrl.dismiss();
+  }
+
+  loadSuggested(ev) {
+    let val = ev.target.value;
+    console.log("This is what is being searched: " + ev)
+    this.getDishes(val)
+  }
+
+  getDishes(val) {
+    let seq = this.api.get('dish/search?search=' + val)
+    seq
+      .map(res => res.json())
+      .subscribe(res => {
+        this.dishResults = res;
+      }, err => {
+        console.error('ERROR', err);
+        this.api.handleError(err);
+      })
   }
 
   goBackToSearch(event, item) {
