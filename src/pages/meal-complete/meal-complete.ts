380ee72287089f1959801b71f3249e4ab0a4ba45
Vanessa Frost <vafrost@ncsu.edu>
2017-05-01 15:07:40 -0400

    replaced with master version

--- src/pages/meal-complete/meal-complete.ts
+++ src/pages/meal-complete/meal-complete.ts
@@ -2,31 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Events } from 'ionic-angular';
 import { MainPage } from '../../pages/pages';
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
-
-// Import ionic2-rating module
-import { Ionic2RatingModule } from 'ionic2-rating';
 
 import { SimpleGlobal } from 'ng2-simple-global';
-
-@NgModule({
-  declarations: [
-    Component,
-    MainPage
-  ],
-  imports: [
-    IonicModule.forRoot(Component),
-    Ionic2RatingModule
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    Component,
-    MainPage
-  ],
-  providers: []
-})
-export class AppModule {}
 
 @Component({
   selector: 'meal-complete',
