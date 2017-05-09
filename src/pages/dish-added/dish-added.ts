d38f2138a58470c5a2c8c7fa262fee6f60e52dec
Ryan Hefner <rhefner1@gmail.com>
2017-05-08 21:05:39 -0400

    Recommended dish (#77)
    * Made place holders for the suggested dishes
    
    * Displays suggested meals using mocked up real data
    
    * Fix some stuff

--- src/pages/dish-added/dish-added.ts
+++ src/pages/dish-added/dish-added.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 import { NavController, ViewController } from 'ionic-angular';
 import { Events } from 'ionic-angular';
 
+import { Api } from '../../providers/api';
+import { DishInfo } from '../dish-info/dish-info';
+
 import { SelectDish } from '../select-dish/select-dish';
+import { SimpleGlobal } from 'ng2-simple-global';
 
 @Component({
   selector: 'page-dish-added',
@@ -10,11 +14,44 @@ import { SelectDish } from '../select-dish/select-dish';
 })
 export class DishAdded {
 
+  suggestedDishes: any = [];
+
   constructor(
+    private api: Api,
     private navCtrl: NavController,
     private viewCtrl: ViewController,
-    private events: Events
-  ) {}
+    private events: Events,
+    private sg: SimpleGlobal
+  ) {
+    this.getDishes();
+  }
+
+  getDishes() {
+    let seq = this.api.get('dish/suggested')
+    seq
+      .map(res => res.json())
+      .subscribe(res => {
+        this.suggestedDishes = res;
+      }, err => {
+        console.error('ERROR', err);
+        this.api.handleError(err);
+      })
+  }
+
+  checkIfAdded(dish) {
+    for (let selectedDish of this.sg['dishes']) {
+      if (dish['id'] == selectedDish['id']) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  openItem(item: any) {
+    this.navCtrl.push(DishInfo, {
+      item_id: item['id']
+    });
+  }
 
   goToMainPage() {
     this.viewCtrl.dismiss();
