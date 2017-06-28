998d1be48394c9ec8d0e171454e3a1f540c49241
Ryan Hefner <rhefner1@gmail.com>
2017-06-28 16:49:20 +0200

    Fix GCP SDK (#79)

--- ci/install.sh
+++ ci/install.sh
@@ -2,12 +2,10 @@
 
 echo -e "\n## Installing Google Cloud SDK"
 
-# If the SDK is not already cached, download it and unpack it
-if [ ! -d ${HOME}/google-cloud-sdk/bin ]; then
-    rm -rf ${HOME}/google-cloud-sdk;
-    export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
-    curl https://sdk.cloud.google.com | bash;
-fi
+# Install GCP SDK
+rm -rf ${HOME}/google-cloud-sdk;
+export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
+curl https://sdk.cloud.google.com | bash;
 
 echo -e "\n## Authenticating to Google Cloud Platform"
 # Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
